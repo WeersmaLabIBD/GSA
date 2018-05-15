6ef5c5b4a25c6251e8f97634c2f971094212a9d5
VoskuilMD <33254072+VoskuilMD@users.noreply.github.com>
2018-05-15 13:07:43 +0200

    Update combined_phewas_manhattanplot.r

--- Tools/combined_phewas_manhattanplot.r
+++ Tools/combined_phewas_manhattanplot.r
@@ -1,7 +1,7 @@
 library(PheWAS)
 
 # Read combined results
-results = read.csv('phewasresults/PheWas_All_snps_no_NA.csv', sep = ',', colClasses=c("phenotype"="character"))
+results = read.csv('PheWas_All_snps_without_NA.csv', sep = ',', colClasses=c("phenotype"="character"))
 
 # Supporting files for plot
 
@@ -13,8 +13,8 @@ phemap[3]=NULL
 phemap[3]=NULL
 
 # Plot results
-pdf("phewasresults/PheWas_All_snps.pdf")
+pdf("PheWas_All_snps.pdf")
 phewasManhattan(results, annotate.phenotype.description = T, title = "PheWas All snps")
 dev.off()
-jpeg("phewasresults/PheWas_All_snps.jpg")
+jpeg("PheWas_All_snps.jpg")
 phewasManhattan(results, annotate.phenotype.description = T, title = "PheWAS All snps")
