8b5596f627dd100399248df4f3e8de784bf24a65
VoskuilMD <33254072+VoskuilMD@users.noreply.github.com>
2018-05-17 11:44:19 +0200

    Update create_phewas_jobs.sh

--- Tools/create_phewas_jobs.sh
+++ Tools/create_phewas_jobs.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-#check number of chunks you have created and enter here
-for i in {000..154};
-do
-echo "#!/bin/bash" >> PheWas_chunk_"$i".sh
-echo "#SBATCH --job-name=PheWAS_5c_chunk_"$i"" >> PheWas_chunk_"$i".sh
-echo "#SBATCH --mem 10gb" >> PheWas_chunk_"$i".sh
-echo "#SBATCH --time=23:59:00" >> PheWas_chunk_"$i".sh
-echo "#SBATCH --output=PheWAS_5c_chunk_"$i".out" >> PheWas_chunk_"$i".sh
-echo "#SBATCH --error=PheWAS_5c_chunk_"$i".err" >> PheWas_chunk_"$i".sh
-echo "#SBATCH --nodes 1" >> PheWas_chunk_"$i".sh
-echo "#SBATCH --cpus-per-task=2" >> PheWas_chunk_"$i".sh
-echo "cd /groups/umcg-weersma/tmp04/Michiel/GSA-redo/phewas/PheWASanalysis/chunks" >> PheWas_chunk_"$i".sh
-echo "module load R" >> PheWas_chunk_"$i".sh
-echo "Rscript PheWas_chunk_"$i".r" >> PheWas_chunk_"$i".sh;
+
+#set your working directory here:
+wd=/groups/umcg-weersma/tmp03/Michiel/chunks
+cd $wd/splits
+ls * | while read line;
+    do
+
+echo "#!/bin/bash" >> ${line}.sh
+echo "#SBATCH --job-name=${line}" >> ${line}.sh
+echo "#SBATCH --mem 10gb" >> ${line}.sh
+echo "#SBATCH --time=23:59:00" >> ${line}.sh
+echo "#SBATCH --output=${line}.out" >> ${line}.sh
+echo "#SBATCH --error=${line}.err" >> ${line}.sh
+echo "#SBATCH --nodes 1" >> ${line}.sh
+echo "#SBATCH --cpus-per-task=2" >> ${line}.sh
+echo "cd $wd" >> ${line}.sh
+echo "module load R" >> ${line}.sh
+echo "Rscript ${line}.r" >> ${line}.sh
+
 done
