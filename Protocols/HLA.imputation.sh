d7682b07b7caccc193e5bbbfa6a1ad90d9ad6378
shixianhu <32686413+shixianhu@users.noreply.github.com>
2021-03-06 12:46:55 +0100

    Create HLA.imputation.sh

---
+++ Protocols/HLA.imputation.sh
@@ -0,0 +1,19 @@
+# Author: GSA HLA imputation
+
+1. Note
+---------------------------------------------------
+
+(1) reference panel, T1DDB
+
+(2) tool, CookHLA (https://github.com/WansonChoi/CookHLA)
+
+(3) imputation region: 25MB-34MB
+
+2. Load enviroment
+---------------------------------------------------
+```
+# export enviroment, otherwise it will return error, e.g. No module named 'pyliftover'
+export PYTHONPATH="/home/umcg-hushixian/miniconda3/envs/CookHLA/lib/python3.6/site-packages/"
+```
+
+3. Do what CookHLA suggest
